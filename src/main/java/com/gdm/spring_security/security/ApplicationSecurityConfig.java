25948b28f8fadf23b790758ca5be39aca64f1a2c
Danielgdm-vlr <daniel.dany16@yahoo.com>
2022-04-22 13:35:23 +0300

    5th commit - BCrypt password encoder

--- src/main/java/com/gdm/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/gdm/spring_security/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.gdm.spring_security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,5 +38,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 // mechanism that enforces the authenticity of a client is by basic auth
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        final UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                // ROLE_STUDENT
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
